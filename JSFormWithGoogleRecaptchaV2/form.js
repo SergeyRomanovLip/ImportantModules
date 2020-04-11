72b657f7729469b8d62b5d7a3e49068662669f11
Sergey Romanov <nashe.dobrov@gmail.com>
2020-04-11 13:43:27 +0300

    commit

---
+++ JSFormWithGoogleRecaptchaV2/form.js
@@ -0,0 +1,92 @@
+// This form will occur in DOM with id="containeer"
+// It send to server by AJAX request (Jquire is necessary) 3 items: name, email and reCaptcha result
+
+window.modal = {
+  class: "modal",
+  id: "modal",
+};
+window.modalForm = {
+  class: "modalForm",
+  id: "modalForm",
+};
+window.inputName = {
+  class: "inputName",
+  type: "text",
+  id: "inputName",
+  name: "name", 
+  placeholder: "Ваше имя",
+};
+window.inputText = {
+  class: "inputText",
+  id: "inputText",
+  name: "text",
+  placeholder: "Текст вашего предложения",
+};
+window.submit = {
+  class: "inputSubmit",
+  type: "submit",
+  id: "inputSubmit",
+  name: "inputSubmit",
+  value: "Отправить",
+  onclick: "sendMessage()",
+};
+function CreateForm() {
+  var modal = document.createElement("div");
+  var modalForm = document.createElement("div");
+  setAttributes(modal, window.modal);
+  setAttributes(modalForm, window.modalForm);
+  document.getElementById("container").appendChild(modal);
+  document.getElementById("modal").appendChild(modalForm);
+  CreateInputName();
+  CreateInputText();
+  CreateSubmit();
+  recaptchaCallback();
+}
+function CreateInputName() {
+  var inputName = document.createElement("input");
+  setAttributes(inputName, window.inputName);
+  document.getElementById("modalForm").appendChild(inputName);
+}
+function CreateInputText() {
+  var inputText = document.createElement("textarea");
+  setAttributes(inputText, window.inputText);
+  document.getElementById("modalForm").appendChild(inputText);
+}
+function CreateSubmit() {
+  var submit = document.createElement("input");
+  setAttributes(submit, window.submit);
+  document.getElementById("modalForm").appendChild(submit);
+}
+function setAttributes(el, attrs) {
+  for (var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+}
+function sendMessage() {
+  var namer = document.getElementById("inputName").value;
+  var textr = document.getElementById("inputText").value;
+  send = $.ajax({
+    type: "POST",
+    url: "https://rsvisualstudio.ru/app/email.php",
+    dataType: "json",
+    data: 'name=' + namer + '&text=' + textr + "&g-recaptcha-response=" + grecaptcha.getResponse(),
+    success: function (data) {
+      var result = data;
+      alert(result);
+      closeModal();
+    },
+  });
+}
+function closeModal() {
+  document.getElementById("modal").remove();
+}
+function recaptchaCallback() {
+  document.getElementById("modalForm").innerHTML +=
+    '<div class="recaptcha" id="recaptcha"></div>';
+  grecaptcha.render("recaptcha", {
+    sitekey: "6LcrL-gUAAAAANhVP-7NkE5Qx3WPqSuDDxkDXWAn",
+    callback: function () {
+    },
+  });
+}
+
---
+++ JSFormWithGoogleRecaptchaV2/main.php
@@ -0,0 +1,34 @@
+
+<?php
+$recaptcha = $_POST['g-recaptcha-response'];
+
+
+    if(!empty($recaptcha))
+        {
+            $secret='6LcrL-gUAAAAAIYUK0zUUJwdUowfBHQIJquvwGqA';
+            $res=file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$recaptcha);
+            $res= json_decode($res, true);
+
+            $name = $_POST['name'];
+            $text = $_POST['text'];
+
+            $name = htmlspecialchars($name);
+            $text = htmlspecialchars($text);
+            $today = date("Y-m-d H:i:s");
+            $to = "sergey.romanov.lip@yandex.ru";
+            $subject = "Обратная связь (с сайта)";
+            $message = '<p><b>Дорогой получатель!</b><br>
+            Вам пришла обратная связь с сайта rsvisualstudio.ru!</p>
+            <p><b>Отправитель:</b> '.$name.'</p>
+            <p><b>Отправлено:</b> '.$today. '</p>
+            <p><b>Текст письма:</b> '.$text.'</p>';
+            $headers  = "Content-type: text/html\r\n"; 
+            $headers .= "From: info@rsvisualstudio.ru\r\n"; 
+            $headers .= "Reply-To: info@rsvisualstudio.ru\r\n"; 
+            if (mail($to, $subject, $message, $headers)) {echo json_encode('Сообщение успешно отправлено, большое спасибо!');}else{echo json_encode('Не удачно');}
+
+}else
+{
+    echo json_encode ('Вы не прошли проверку reCaptcha');
+}
+ ?>
